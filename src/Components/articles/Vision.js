5c1a291c43b51c9b0c6aed1c1e8506fa671c5530
azerckid <75958755+azerckid@users.noreply.github.com>
2021-01-04 18:01:45 +0900

    text update

--- src/Components/articles/Vision.js
+++ src/Components/articles/Vision.js
@@ -53,7 +53,7 @@ const TextBox = styled.div`
   width: 800px;
   height: 200px;
   margin-top: 20px;
-  line-height: 20px;
+  line-height: 25px;
   color: white;
   font-size: 16px;
   text-align: center;
@@ -67,22 +67,22 @@ export default () => {
           <Title>VISION</Title>
           <SubTitle>"Vision 2024 - Lead the Next"</SubTitle>
           <TextBox>
-            He was a linebacker for the Chicago Bears before he turned to
-            another kind of a contact sport – trading on the Chicago Board
-            Options Exchange. He became a member of the CBOE, NYSE, CME, and
-            CBOT and worked as a floor trader for 25 years. In 1990, he founded
-            Mercury Trading, a market-making firm at the Chicago Board Options
-            Exchange (CBOE), which he sold in 2004 to Citadel, one of the
-            world’s largest hedge funds. He was a linebacker for the Chicago
-            Bears before he turned to another kind of a contact sport – trading
-            on the Chicago Board Options Exchange.
+            제시된 제품이나 서비스는 많은 유틸리티 잠재력을 가져야 한다. USFCX는
+            실제 산업 문제를 해결하는 해결책을 찾고 있다. 블록체인 기술을
+            활용하여 경쟁우위를 창출한다. 제품 개발이 진행될수록 고객이 더욱
+            활발해집니다 비즈니스 프로세스에 대한 정보를 더 많이 제공하는 관련
+            파트너가 많을수록 법적 프레임워크가 개선됩니다 설계됨 - 일반
+            비즈니스 제안 중에서 보석을 식별하는 것이 더 쉽습니다. 백서는 ...로
+            발전했다 업계 표준과 백서의 품질은 우리의 검토 과정에서 많은 비중을
+            두고 있다. 백서는 많은 정보를 압축하고, 실제 고객 문제를 해결하는
+            팀임을 나타내며, 활용된 기술을 설명할 수 있으며, 충분히 소통되어
+            대중의 매력을 얻을 수 있는 제안이다.
           </TextBox>
           <Title style={{ marginTop: "50px" }}>MISSION</Title>
           <TextBox style={{ marginTop: "0px" }}>
-            He was a linebacker for the Chicago Bears before he turned to
-            another kind of a contact sport – trading on the Chicago Board
-            Options Exchange. He became a member of the CBOE, NYSE, CME, and
-            CBOT and worked as a floor trader for 25 years.
+            USFCX에서 우리는 고객들이 우리와의 금융 정점에 도달하기 위해 우선
+            순위를 매기고 싶다. 어떻게 해야 하는지의 과정을 바꿀 수 있게 디지털
+            트랜잭션은 글로벌 방식으로 처리됩니다.
           </TextBox>
         </ContextContainer>
       </BackImage>
--- src/Components/articles/WhatIs.js
+++ src/Components/articles/WhatIs.js
@@ -45,7 +45,7 @@ const TextBox = styled.div`
   width: 800px;
   height: 200px;
   margin-top: 170px;
-  line-height: 20px;
+  line-height: 25px;
   color: white;
   font-size: 16px;
   text-align: center;
@@ -58,23 +58,19 @@ export default () => {
         <ContextContainer>
           <Title>What is USFCX?</Title>
           <TextBox>
-            He was a linebacker for the Chicago Bears before he turned to
-            another kind of a contact sport – trading on the Chicago Board
-            Options Exchange. He became a member of the CBOE, NYSE, CME, and
-            CBOT and worked as a floor trader for 25 years. In 1990, he founded
-            Mercury Trading, a market-making firm at the Chicago Board Options
-            Exchange (CBOE), which he sold in 2004 to Citadel, one of the
-            world’s largest hedge funds. He was a linebacker for the Chicago
-            Bears before he turned to another kind of a contact sport – trading
-            on the Chicago Board Options Exchange. He became a member of the
-            CBOE, NYSE, CME, and CBOT and worked as a floor trader for 25 years.
-            In 1990, he founded Mercury Trading, a market-making firm at the
-            Chicago Board Options Exchange (CBOE), which he sold in 2004 to
-            Citadel, one of the world’s largest hedge funds. He became a member
-            of the CBOE, NYSE, CME, and CBOT and worked as a floor trader for 25
-            years. In 1990, he founded Mercury Trading, a market-making firm at
-            the Chicago Board Options Exchange (CBOE), which he sold in 2004 to
-            Citadel, one of the world’s largest hedge funds.
+            회사 미국 파운드리 암호화폐 거래소는 블록체인 기술의 선두주자다.
+            통화시장의 추세가 지속적으로 성장하고 있기 때문이다. 이 회사의
+            비전은 글로벌화와 함께 비즈니스 거래, 보안을 개선할 수 있는 플랫폼을
+            제공하는 것이다. 수집 및 투자 채널이 구축되기 위해 필요한 데이터가
+            점점 더 많아지고 있다. FXT, 비트코인, 에테르, 리테코인과 같이
+            채굴되고 있는 동전의 디지털 자산에 대한 기술을 전환한다. 또한 새로운
+            것의 출현과 함께 소프트웨어 엔지니어 연구팀에 자금을 지원하여
+            플랫폼을 구축하고 거래 시스템의 처리를 신속히 할 수 있습니다. 가격
+            상승의 재정적 돌파구는 디지털 커뮤니티에 흥미진진한 미래를
+            만들어준다. 투자자들은 안전한 암호화폐 거래를 위해 홍보하고
+            업그레이드하기를 원한다. 모든 고객은 가장 존경받고 가치 있는 대우를
+            받는다. 그러나 가장 중요한 측면은 사물을 안전하고 단순하게 유지하는
+            것이다
           </TextBox>
         </ContextContainer>
       </BackImage>
