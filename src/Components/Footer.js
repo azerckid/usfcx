b276d47c5504db8171ec2cfd1b0a5a91f55b0b78
azerckid <75958755+azerckid@users.noreply.github.com>
2021-01-04 15:16:26 +0900

    ....

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,31 +2,69 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  width: 100%;
   height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: black;
   color: white;
+`;
+
+const Title = styled.div`
+  margin: 20px 0px;
+  font-size: 20px;
+  color: white;
+`;
+
+const UnderMenuNaviContainer = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0px;
+`;
+
+const SnsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+const IconBoxContainer = styled.div`
+  width: 20%;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: space-around;
+`;
+const IconBox = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: gray;
 `;
 
 export default () => {
   return (
     <Container>
-      <div>USFCX</div>
-      <div>
-        <span>AAAAA</span>
-        <span>BBBBB</span>
-        <span>CCCCC</span>
-        <span>DDDDD</span>
-        <span>EEEEE</span>
-      </div>
-      <div>
-        <div>
-          <span>AAAAA</span>
-          <span>BBBBB</span>
-          <span>CCCCC</span>
-          <span>DDDDD</span>
-        </div>
+      <Title>USFCX</Title>
+      <UnderMenuNaviContainer>
+        <div>AAAAA</div>
+        <div>BBBBB</div>
+        <div>CCCCC</div>
+        <div>DDDDD</div>
+        <div>EEEEE</div>
+      </UnderMenuNaviContainer>
+      <SnsContainer>
+        <IconBoxContainer>
+          <IconBox></IconBox>
+          <IconBox></IconBox>
+          <IconBox></IconBox>
+          <IconBox></IconBox>
+        </IconBoxContainer>
         <span>copyright</span>
-      </div>
+      </SnsContainer>
     </Container>
   );
 };
